b1d592cd9065f10a08767d323ca96e74daf0fafe
Ikymuco <ikymuco@gmail.com>
2026-09-18 12:13:22 +0600

    Simple Work v0: add minimal work session surface

---
+++ src/codexia_manual_agent/simple_work/__init__.py
@@ -0,0 +1,17 @@
+from codexia_manual_agent.simple_work.runtime import (
+    SimpleWorkRunResult,
+    SimpleWorkRuntime,
+)
+from codexia_manual_agent.simple_work.session import (
+    SimpleWorkSession,
+    SimpleWorkStatus,
+    SimpleWorkStore,
+)
+
+__all__ = [
+    "SimpleWorkRunResult",
+    "SimpleWorkRuntime",
+    "SimpleWorkSession",
+    "SimpleWorkStatus",
+    "SimpleWorkStore",
+]
