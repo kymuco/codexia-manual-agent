b33aff148d13153a8546f1ffa5958652f2a0e18a
Ikymuco <ikymuco@gmail.com>
2026-09-18 13:51:41 +0600

    Simple Work v0.1: export shared-session contracts

--- src/codexia_manual_agent/simple_work/__init__.py
+++ src/codexia_manual_agent/simple_work/__init__.py
@@ -3,15 +3,21 @@ from codexia_manual_agent.simple_work.runtime import (
     SimpleWorkRuntime,
 )
 from codexia_manual_agent.simple_work.session import (
+    SimpleCodexiaSession,
+    SimpleWorkEvent,
     SimpleWorkSession,
     SimpleWorkStatus,
     SimpleWorkStore,
+    WorkerMode,
 )
 
 __all__ = [
+    "SimpleCodexiaSession",
+    "SimpleWorkEvent",
     "SimpleWorkRunResult",
     "SimpleWorkRuntime",
     "SimpleWorkSession",
     "SimpleWorkStatus",
     "SimpleWorkStore",
+    "WorkerMode",
 ]
