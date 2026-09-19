0d0cdc439d419b995e6262c62cc4646e3673b7a1
Ikymuco <ikymuco@gmail.com>
2026-09-19 17:45:46 +0600

    G2.6: expose standalone host adapter

---
+++ src/codexia_manual_agent/standalone_host/__init__.py
@@ -0,0 +1,17 @@
+from codexia_manual_agent.standalone_host.process_capability import (
+    PROCESS_CAPABILITY_ID,
+    PROCESS_CAPABILITY_VERSION,
+    PROCESS_OPERATION,
+    STANDALONE_PROCESS_HOST_ID,
+    StandaloneProcessCapabilityPort,
+    StandaloneProcessHostError,
+)
+
+__all__ = [
+    "PROCESS_CAPABILITY_ID",
+    "PROCESS_CAPABILITY_VERSION",
+    "PROCESS_OPERATION",
+    "STANDALONE_PROCESS_HOST_ID",
+    "StandaloneProcessCapabilityPort",
+    "StandaloneProcessHostError",
+]
