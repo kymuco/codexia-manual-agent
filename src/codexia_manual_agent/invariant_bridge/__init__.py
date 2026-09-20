395ba5b05677b4afd9850ecf037dbb80f3ebba95
Ikymuco <ikymuco@gmail.com>
2026-09-20 13:35:11 +0600

    G2.8: expose Invariant Pack bridge

---
+++ src/codexia_manual_agent/invariant_bridge/__init__.py
@@ -0,0 +1,15 @@
+from codexia_manual_agent.invariant_bridge.pack_distribution import (
+    PACK_DISTRIBUTION_SCHEMA_VERSION,
+    InvariantPackDistributionBridge,
+    InvariantPackDistributionError,
+    ManagedPluginServicePort,
+    ResolvedPackDistribution,
+)
+
+__all__ = [
+    "PACK_DISTRIBUTION_SCHEMA_VERSION",
+    "InvariantPackDistributionBridge",
+    "InvariantPackDistributionError",
+    "ManagedPluginServicePort",
+    "ResolvedPackDistribution",
+]
