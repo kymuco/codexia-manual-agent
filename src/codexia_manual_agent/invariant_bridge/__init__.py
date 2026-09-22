ca0c4170eb38e2926e1bf7c381a0ae3a90c229be
Ikymuco <ikymuco@gmail.com>
2026-09-22 20:15:58 +0600

    G2.10: normalize Invariant bridge exports

--- src/codexia_manual_agent/invariant_bridge/__init__.py
+++ src/codexia_manual_agent/invariant_bridge/__init__.py
@@ -5,21 +5,6 @@ from codexia_manual_agent.invariant_bridge.pack_distribution import (
     ManagedPluginServicePort,
     ResolvedPackDistribution,
 )
-
-__all__ = [
-    "PACK_DISTRIBUTION_SCHEMA_VERSION",
-    "InvariantPackDistributionBridge",
-    "InvariantPackDistributionError",
-    "ManagedPluginServicePort",
-    "ResolvedPackDistribution",
-    "WORKFLOW_IMPLEMENTATION_EXPORT",
-    "InvariantWorkflowImplementationBindingError",
-    "InvariantWorkflowImplementationBridge",
-    "InvariantWorkflowImplementationError",
-    "InvariantWorkflowImplementationShapeError",
-    "ResolvedWorkflowImplementation",
-]
-
 from codexia_manual_agent.invariant_bridge.workflow_implementation import (
     WORKFLOW_IMPLEMENTATION_EXPORT,
     InvariantWorkflowImplementationBindingError,
@@ -28,3 +13,17 @@ from codexia_manual_agent.invariant_bridge.workflow_implementation import (
     InvariantWorkflowImplementationShapeError,
     ResolvedWorkflowImplementation,
 )
+
+__all__ = [
+    "PACK_DISTRIBUTION_SCHEMA_VERSION",
+    "WORKFLOW_IMPLEMENTATION_EXPORT",
+    "InvariantPackDistributionBridge",
+    "InvariantPackDistributionError",
+    "InvariantWorkflowImplementationBindingError",
+    "InvariantWorkflowImplementationBridge",
+    "InvariantWorkflowImplementationError",
+    "InvariantWorkflowImplementationShapeError",
+    "ManagedPluginServicePort",
+    "ResolvedPackDistribution",
+    "ResolvedWorkflowImplementation",
+]
