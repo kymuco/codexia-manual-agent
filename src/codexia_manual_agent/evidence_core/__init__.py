7c30fe6524f75dfedabfdf4af2b01912defb518b
Ikymuco <ikymuco@gmail.com>
2026-09-25 14:40:47 +0600

    G2.32 expose EvidenceRef contract

---
+++ src/codexia_manual_agent/evidence_core/__init__.py
@@ -0,0 +1,11 @@
+from codexia_manual_agent.evidence_core.models import (
+    EVIDENCE_REF_SCHEMA_VERSION,
+    EvidenceRef,
+    InvalidEvidenceRef,
+)
+
+__all__ = [
+    "EVIDENCE_REF_SCHEMA_VERSION",
+    "EvidenceRef",
+    "InvalidEvidenceRef",
+]
