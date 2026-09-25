13e441528c3595b355898a67c4dc9edd28568656
Ikymuco <ikymuco@gmail.com>
2026-09-25 18:57:34 +0600

    G2.40 expose completion resolver requirement error

--- src/codexia_manual_agent/workflow_orchestration/__init__.py
+++ src/codexia_manual_agent/workflow_orchestration/__init__.py
@@ -6,6 +6,7 @@ from codexia_manual_agent.workflow_orchestration.cognition_progression import (
 )
 from codexia_manual_agent.workflow_orchestration.proposal_admission import (
     WorkflowProposalAdmissionBindingError,
+    WorkflowProposalAdmissionCompletionResolverRequiredError,
     WorkflowProposalAdmissionError,
     WorkflowProposalAdmissionPackRequiredError,
     WorkflowProposalAdmissionResult,
@@ -51,6 +52,7 @@ __all__ = [
     "RoleCognitionStateError",
     "RoleInstructionsMaterialPort",
     "WorkflowProposalAdmissionBindingError",
+    "WorkflowProposalAdmissionCompletionResolverRequiredError",
     "WorkflowProposalAdmissionError",
     "WorkflowProposalAdmissionPackRequiredError",
     "WorkflowProposalAdmissionResult",
