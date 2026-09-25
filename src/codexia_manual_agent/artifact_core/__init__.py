fda4ea8ef3f22bc6e080a3c4b2894e5ef02691ba
Ikymuco <ikymuco@gmail.com>
2026-09-25 14:02:14 +0600

    G2.30 expose artifact relation boundary

--- src/codexia_manual_agent/artifact_core/__init__.py
+++ src/codexia_manual_agent/artifact_core/__init__.py
@@ -1,11 +1,33 @@
+from codexia_manual_agent.artifact_core.admission import (
+    ArtifactAdmission,
+    ArtifactAdmissionError,
+    ArtifactBindingError,
+    ArtifactIdentityConflictError,
+    ArtifactStateError,
+)
 from codexia_manual_agent.artifact_core.models import (
+    ARTIFACT_REF_RECORDED_EVENT,
     ARTIFACT_REF_SCHEMA_VERSION,
     ArtifactRef,
     InvalidArtifactRef,
 )
 
 __all__ = [
+    "ARTIFACT_REF_RECORDED_EVENT",
+    "ArtifactAdmission",
+    "ArtifactAdmissionError",
+    "ArtifactBindingError",
+    "ArtifactIdentityConflictError",
+    "ArtifactProjectionError",
+    "ArtifactStateError",
     "ARTIFACT_REF_SCHEMA_VERSION",
     "ArtifactRef",
     "InvalidArtifactRef",
+    "project_artifact_ref",
+    "project_artifact_refs",
 ]
+from codexia_manual_agent.artifact_core.projection import (
+    ArtifactProjectionError,
+    project_artifact_ref,
+    project_artifact_refs,
+)
