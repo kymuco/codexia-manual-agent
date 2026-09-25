79b886dbd77e7614c5cb16943bceb7a3069d09d6
Ikymuco <ikymuco@gmail.com>
2026-09-25 13:42:53 +0600

    G2.29 expose ArtifactRef contract

---
+++ src/codexia_manual_agent/artifact_core/__init__.py
@@ -0,0 +1,11 @@
+from codexia_manual_agent.artifact_core.models import (
+    ARTIFACT_REF_SCHEMA_VERSION,
+    ArtifactRef,
+    InvalidArtifactRef,
+)
+
+__all__ = [
+    "ARTIFACT_REF_SCHEMA_VERSION",
+    "ArtifactRef",
+    "InvalidArtifactRef",
+]
