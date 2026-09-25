0b5050a89a981b70c00898ed376ad8619fe7eeea
Ikymuco <ikymuco@gmail.com>
2026-09-25 15:47:48 +0600

    G2.35 expose CompletionClaim contract

---
+++ src/codexia_manual_agent/completion_core/__init__.py
@@ -0,0 +1,11 @@
+from codexia_manual_agent.completion_core.models import (
+    COMPLETION_CLAIM_SCHEMA_VERSION,
+    CompletionClaim,
+    InvalidCompletionClaim,
+)
+
+__all__ = [
+    "COMPLETION_CLAIM_SCHEMA_VERSION",
+    "CompletionClaim",
+    "InvalidCompletionClaim",
+]
