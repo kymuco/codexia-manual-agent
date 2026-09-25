4779d0f213331aa215e168bec958f1416f98df23
Ikymuco <ikymuco@gmail.com>
2026-09-25 14:03:40 +0600

    G2.30 normalize artifact Core exports

--- src/codexia_manual_agent/artifact_core/__init__.py
+++ src/codexia_manual_agent/artifact_core/__init__.py
@@ -11,23 +11,23 @@ from codexia_manual_agent.artifact_core.models import (
     ArtifactRef,
     InvalidArtifactRef,
 )
-
-__all__ = [
-    "ARTIFACT_REF_RECORDED_EVENT",
-    "ArtifactAdmission",
-    "ArtifactAdmissionError",
-    "ArtifactBindingError",
-    "ArtifactIdentityConflictError",
-    "ArtifactProjectionError",
-    "ArtifactStateError",
-    "ARTIFACT_REF_SCHEMA_VERSION",
-    "ArtifactRef",
-    "InvalidArtifactRef",
-    "project_artifact_ref",
-    "project_artifact_refs",
-]
 from codexia_manual_agent.artifact_core.projection import (
     ArtifactProjectionError,
     project_artifact_ref,
     project_artifact_refs,
 )
+
+__all__ = [
+    "ARTIFACT_REF_RECORDED_EVENT",
+    "ARTIFACT_REF_SCHEMA_VERSION",
+    "ArtifactAdmission",
+    "ArtifactAdmissionError",
+    "ArtifactBindingError",
+    "ArtifactIdentityConflictError",
+    "ArtifactProjectionError",
+    "ArtifactRef",
+    "ArtifactStateError",
+    "InvalidArtifactRef",
+    "project_artifact_ref",
+    "project_artifact_refs",
+]
