49a53377bbdabc558613b8f70709337d2616d6b6
Ikymuco <ikymuco@gmail.com>
2026-09-25 15:05:13 +0600

    G2.33 expose evidence relation boundary

--- src/codexia_manual_agent/evidence_core/__init__.py
+++ src/codexia_manual_agent/evidence_core/__init__.py
@@ -1,11 +1,33 @@
+from codexia_manual_agent.evidence_core.admission import (
+    EvidenceAdmission,
+    EvidenceAdmissionError,
+    EvidenceBindingError,
+    EvidenceIdentityConflictError,
+    EvidenceStateError,
+)
 from codexia_manual_agent.evidence_core.models import (
+    EVIDENCE_REF_RECORDED_EVENT,
     EVIDENCE_REF_SCHEMA_VERSION,
     EvidenceRef,
     InvalidEvidenceRef,
 )
+from codexia_manual_agent.evidence_core.projection import (
+    EvidenceProjectionError,
+    project_evidence_ref,
+    project_evidence_refs,
+)
 
 __all__ = [
+    "EVIDENCE_REF_RECORDED_EVENT",
     "EVIDENCE_REF_SCHEMA_VERSION",
+    "EvidenceAdmission",
+    "EvidenceAdmissionError",
+    "EvidenceBindingError",
+    "EvidenceIdentityConflictError",
+    "EvidenceProjectionError",
     "EvidenceRef",
+    "EvidenceStateError",
     "InvalidEvidenceRef",
+    "project_evidence_ref",
+    "project_evidence_refs",
 ]
